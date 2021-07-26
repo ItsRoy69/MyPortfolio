bb953f4123b445596c26da7927969d07d927f736
Jyotirmoy Roy <78967360+ItsRoy69@users.noreply.github.com>
2021-07-26 14:42:09 +0530

    My Portfolio Maybe

---
+++ script.js
@@ -0,0 +1,91 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function () {
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: [ "Developer", "Designer",  "Coder"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Designer", "Coder" ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-3", {
+        strings: [" have passed my Class X from Meghmala Roy Education Centre !", " have passed my Class XI from St.Joseph & Mary's School !", " am graduating from University of Engineering & Management, Kolkata !" ],
+        typeSpeed: 80,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+
+// why engineering
+const soundBtn = document.querySelector('.play-sound-btn');
+let myAudio = document.querySelector('#audio');
+soundBtn.addEventListener('click',()=>{
+  myAudio.play();
+});
