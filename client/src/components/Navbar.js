7f9fcc7ffccd7ef65d278280eec109760c8f8967
ItsRoy69 <jyotirmoyroy649@gmail.com>
2022-05-26 14:33:16 +0530

    Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,23 +7,18 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="navbar">
-        <div className="max-width">
-          <div className="logo">
-            <h1 href="#">Portfo<span>lio.</span></h1>
-          </div>
-          <ul className="menu">
-            <li><a href="#home" className="menu-btn">Home</a></li>
-            <li><a href="#about" className="menu-btn">About</a></li>
-            <li><a href="#skills" className="menu-btn">Skills</a></li>
-            <li><a href="#achievements" className="menu-btn">Achievements</a></li>
-            <li><a href="#contact" className="menu-btn">Contact</a></li>
-          </ul>
-          <div className="menu-btn">
-            <i className="fas fa-bars"></i>
-          </div>
+      <div className="navbar">
+        <div className="logo">
+          <h1 href="#">Portfo<span>lio.</span></h1>
         </div>
-      </nav>
+        <ul className="menu">
+          <li><a href="#home" className="menu-btn">Home</a></li>
+          <li><a href="#about" className="menu-btn">About</a></li>
+          <li><a href="#skills" className="menu-btn">Skills</a></li>
+          <li><a href="#achievements" className="menu-btn">Work</a></li>
+          <li><a href="#contact" className="menu-btn">Contact</a></li>
+        </ul>
+      </div>
     </>
   )
 }
